dbb15c70a36beab0d3123f0ead03382958891640
Nitin Kumar <snitin315@gmail.com>
2021-04-05 07:13:52 +0530

    test: snapshot devserver-4 webpack-4

--- scripts/snapshotResolver.js
+++ scripts/snapshotResolver.js
@@ -1,12 +1,21 @@
 const path = require('path');
 
 const webpack = require('webpack');
-
+//eslint-disable-next-line node/no-unpublished-require
+const [devServerVersion] = require('webpack-dev-server/package.json').version;
 const [webpackVersion] = webpack.version;
+
 const snapshotExtension = `.snap.webpack${webpackVersion}`;
+const snapshotExtensionForServe = `.snap.devServer${devServerVersion}.webpack${webpackVersion}`;
 
 module.exports = {
-    resolveSnapshotPath: (testPath) => path.join(path.dirname(testPath), '__snapshots__', `${path.basename(testPath)}${snapshotExtension}`),
+    resolveSnapshotPath: (testPath) => {
+        if (testPath.includes('serve')) {
+            return path.join(path.dirname(testPath), '__snapshots__', `${path.basename(testPath)}${snapshotExtensionForServe}`);
+        } else {
+            return path.join(path.dirname(testPath), '__snapshots__', `${path.basename(testPath)}${snapshotExtension}`);
+        }
+    },
     resolveTestPath: (snapshotPath) => snapshotPath.replace(`${path.sep}__snapshots__`, '').slice(0, -snapshotExtension.length),
     testPathForConsistencyCheck: path.join('consistency_check', '__tests__', 'example.test.js'),
 };
