f237e4b355c8026ce8595d4f669680daf928a586
evilebottnawi <sheo13666q@gmail.com>
2021-04-07 18:04:40 +0300

    test: update snapshot

--- scripts/snapshotResolver.js
+++ scripts/snapshotResolver.js
@@ -8,13 +8,16 @@ const [webpackVersion] = webpack.version;
 const snapshotExtension = `.snap.webpack${webpackVersion}`;
 const snapshotExtensionForServe = `.snap.devServer${devServerVersion}.webpack${webpackVersion}`;
 
+const helpCommandTestDir = path.resolve(__dirname, '../test/help');
+const serveCommandTestDir = path.resolve(__dirname, '../test/help');
+
 module.exports = {
     resolveSnapshotPath: (testPath) => {
-        if (testPath.includes('serve')) {
+        if (testPath.startsWith(helpCommandTestDir) || testPath.startsWith(serveCommandTestDir)) {
             return path.join(path.dirname(testPath), '__snapshots__', `${path.basename(testPath)}${snapshotExtensionForServe}`);
-        } else {
-            return path.join(path.dirname(testPath), '__snapshots__', `${path.basename(testPath)}${snapshotExtension}`);
         }
+
+        return path.join(path.dirname(testPath), '__snapshots__', `${path.basename(testPath)}${snapshotExtension}`);
     },
     resolveTestPath: (snapshotPath) => snapshotPath.replace(`${path.sep}__snapshots__`, '').slice(0, -snapshotExtension.length),
     testPathForConsistencyCheck: path.join('consistency_check', '__tests__', 'example.test.js'),
